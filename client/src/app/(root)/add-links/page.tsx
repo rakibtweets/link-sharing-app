33833cb0791b17304371ac786c8be6e83169ae33
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-10-11 18:53:33 +0600

    Refactor layout component and add profile phone mockup
    - Refactor the LinkPageLayout component to accept children as props
    - Add the ProfilePhoneMockup component to display a phone mockup on the profile page
    - Update the Logo component to use the primary text color and adjust the size of the link icon
    - Update the AddLinks page to include the ProfilePhoneMockup component and improve the layout

--- client/src/app/(root)/add-links/page.tsx
+++ client/src/app/(root)/add-links/page.tsx
@@ -1,7 +1,21 @@
+import ProfilePhoneMockup from '@/components/ui/ProfilePhoneMockup';
+
 export default function AddLinks() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Add Links page</h1>
-    </div>
+    <section className="grid grid-cols-2 gap-8 pt-14 tablet:grid-cols-1 tablet:pt-0">
+      {/* Profile Phone Mockup */}
+      <ProfilePhoneMockup />
+      {/* Add Links */}
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-3xl font-bold text-center">
+          Add your links and social media
+        </h2>
+        <p className="text-center text-gray-500 mt-4">
+          Add your social media links and give your audience a way to connect
+          with you.
+        </p>
+        <button className="btn-primary mt-8">Add Links</button>
+      </div>
+    </section>
   );
 }
--- client/src/components/ui/Logo.tsx
+++ client/src/components/ui/Logo.tsx
@@ -1,11 +1,10 @@
 import { Link } from 'lucide-react';
 
 const Logo = () => {
-  const isMobile = window.screen.width <= 768;
   return (
-    <div className="flex items-center gap-4 text-[#633cff]">
-      <Link size={isMobile ? '1.6rem' : '2.5rem'} />
-      <h1 className="text-5xl  font-bold">devlinks</h1>
+    <div className="flex items-center gap-4 text-primary">
+      <Link className="mobile:text-[1.6rem] text-[2.5rem]" />
+      <h1 className="text-5xl font-bold">devlinks</h1>
     </div>
   );
 };
